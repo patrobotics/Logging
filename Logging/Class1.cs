ced758a22f2e31593a4fc09aedcef74ec8918b35
cursemark <PatrickT@patrobotics.com>
2022-05-21 17:20:10 -0400

    Add project files.

---
+++ Logging/Class1.cs
@@ -0,0 +1,163 @@
+﻿namespace Logging
+{
+    public class Log
+    {
+        public static void Green(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+        }
+
+        public static void Gray(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+        }
+        public static void Red(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+        }
+        public static void Yellow(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+        }
+        public static void Cyan(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+        }
+        public static void Black(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+
+        }
+        public static void Blue(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+
+        }
+        public static void Purple(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+        }
+        public static void White(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine();
+
+            Console.Write(text);
+        }
+
+        internal class Increment
+        {
+            public static void Green(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+            }
+
+            public static void Gray(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+            }
+            public static void Red(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+            }
+            public static void Yellow(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+            }
+            public static void Cyan(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+            }
+            public static void Black(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+
+            }
+            public static void Blue(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+
+            }
+            public static void Purple(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+            }
+            public static void White(string text)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+            }
+
+        }
+       
+
+    }
+}
